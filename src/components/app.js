b296f21a8b1e917a169d4030d52ded0350255e49
Hilberteah <hilberte302@bucs.dawson.edu>
2019-12-16 09:33:52 -0700

    env var

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+ 
+
+const MY_SECRET_KEY = process.env.MY_SECRET_KEY
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Heroku Push</h1>
+        <h2>{MY_SECRET_KEY}</h2>
+        
+      </div>
+    );
+  }
+}
