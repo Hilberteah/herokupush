f732cbd725b0594dc38c6fba6978cac748a49415
Hilberteah <hilberte302@bucs.dawson.edu>
2019-12-17 10:13:48 -0700

    updated

--- src/components/app.js
+++ src/components/app.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Heroku Push</h1>
+        <h1>Heroku Push!</h1>
         <h2>{MY_SECRET_KEY}</h2>
         
       </div>
